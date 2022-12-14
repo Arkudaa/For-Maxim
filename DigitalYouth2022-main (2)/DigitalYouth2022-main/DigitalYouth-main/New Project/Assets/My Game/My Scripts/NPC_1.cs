fdf1aa2fdc4077e5968566f3f5a8e23d6daf5820
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-12-14 15:54:43 +0200

    version1.0

---
+++ DigitalYouth2022-main (2)/DigitalYouth2022-main/DigitalYouth-main/New Project/Assets/My Game/My Scripts/NPC_1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_1 : NPC {
+	 
+	// Use this for initialization
+	public override void OnSetUpDialogue() {
+		Speech.AddDialogue("0","HELLO TRAVELLER!", "1");
+		Speech.AddDialogue("1", "WELCOME TO OUR PLANET", "2");
+		Speech.AddDialogue("2", "FIND POWERUP AND YOU CAN JUMP HIGHER", "3");
+		Speech.AddDialogue("3", "THEN GO TO NEXT ISLAND");
+	}
+
+	// Triggered when the player comes in range of the NPC
+	public override void OnTriggerNPC( Collider other ){
+		Speech.SetDialogue("0");
+	}
+}
---
+++ DigitalYouth2022-main (2)/DigitalYouth2022-main/DigitalYouth-main/New Project/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public int coin;
+	
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start()
+	{
+		Abilities.doubleJumpEnabled = true;
+	}
+	
+
+	// OnTriggerEnter is called when the Collider "collided" enters the trigger.
+	protected void OnTriggerEnter(Collider collided) {
+
+		// Check for collision with coins
+		if (collided.gameObject.tag == "Coin") {
+
+			Destroy(collided.gameObject);
+			coin++;
+			HUD.UpdateCoinCount(coin);
+
+
+		}
+
+
+		if (collided.gameObject.name == "Powerup1")
+		{
+			Destroy(collided.gameObject);
+			Abilities.doubleJumpEnabled = true;
+			HUD.Message("You got powerup!! You can jump higher!!");
+
+		}
+
+		if (collided.gameObject.name == "Powerup2")
+		{ 
+			Destroy(collided.gameObject);
+		Abilities.spinAttackEnabled = true;
+			HUD.Message("You got powerup!! You can do spin attack!!");
+		}
+	}
+}
